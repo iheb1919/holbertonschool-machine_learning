391f8e0611ce61528e00e4c990f788f142339b92
iheb1919 <ihebmejri14@gmail.com>
2020-08-06 23:03:40 +0100

    ff

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -9,9 +9,11 @@ def cat_matrices2D(mat1, mat2, axis=0):
     7. Gettin’ Cozy
     """
     res = []
+    re = []
     if axis == 0 and (len(mat1[0]) == len(mat2[0])):
         return mat1 + mat2
-    elif axis == 1  and (len(mat1) == len(mat2)):
+    elif axis == 1 and (len(mat1) == len(mat2)):
         for i in range(len(mat1)):
-            res.append(mat1[i] + mat2[i])
+            re = mat1[i] + mat2[i]
+            res.append(re)
     return res
