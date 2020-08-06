fdc0f1194974709ef249320402c6ec5693297141
iheb1919 <ihebmejri14@gmail.com>
2020-08-06 22:52:24 +0100

    fix code

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -9,6 +9,8 @@ def cat_matrices2D(mat1, mat2, axis=0):
     7. Gettin’ Cozy
     """
     res = []
+    if len(mat1) != len(mat2):
+        return None
     if axis == 0:
         return mat1 + mat2
     elif axis == 1:
