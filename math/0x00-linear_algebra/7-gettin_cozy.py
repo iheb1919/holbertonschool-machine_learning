38ad0c38a297139e359d83ee1958c4a5fe7701ba
iheb1919 <ihebmejri14@gmail.com>
2020-08-06 23:06:58 +0100

    ff

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,24 +1,4 @@
-"""#!/usr/bin/env python3
-"""
-'7. Gettin’ Cozy'
-"""
-
-
-def cat_matrices2D(mat1, mat2, axis=0):
-    """
-
-    """
-    res = []
-    re = []
-    if axis == 0 and (len(mat1[0]) == len(mat2[0])):
-        return mat1 + mat2
-    elif axis == 1 and (len(mat1) == len(mat2)):
-        for i in range(len(mat1)):
-            re = mat1[i] + mat2[i]
-            res.append(re)
-    return res
-"""
-    #!/usr/bin/env python3
+#!/usr/bin/env python3
 """copet cat matrice"""
 
 
