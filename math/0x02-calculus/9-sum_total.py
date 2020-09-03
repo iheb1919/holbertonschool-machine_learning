89be36b9a25849119328e3b0fccd1c76494f0764
iheb1919 <ihebmejri14@gmail.com>
2020-09-03 04:00:49 +0100

    ff

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-"""lll"""
+"""sum total"""
+
 
 def summation_i_squared(n):
-    """sum """
+    """sum total"""
     if n is None or n < 1:
         return None
     res = list(map(lambda x: x**2, list(range(1, n+1))))
