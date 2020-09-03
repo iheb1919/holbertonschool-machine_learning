7307b8bf4e469ee403f26be463e1c374e345146b
iheb1919 <ihebmejri14@gmail.com>
2020-09-03 02:45:21 +0100

    ff

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def poly_integral(poly, C=0):
+    """ poly_integral"""
+    res =[C]
+    for i in range(len(poly)):
+        inte = (poly[i] ** i) / (i + 1)
+        if i == 0:
+            res.append(poly[i])
+        elif inte.is_integer():
+            res.append(int(inte))
+        else:
+            res.append(inte)
+    return res
