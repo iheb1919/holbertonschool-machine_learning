b694486439fe5f91e21756104d855095af3be2bf
iheb1919 <ihebmejri14@gmail.com>
2020-09-03 03:02:15 +0100

    ff

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -5,17 +5,15 @@
 def poly_integral(poly, C=0):
     """ poly_integral"""
 
-    if isinstance(poly, list):
+    if type(poly) is not list or type(C) is not int:
         return None
     if poly is None or len(poly) == 0:
-        return None
-    if(type(C) is not int):
         return None
     if(poly == [0]):
         return [C]
     res = [C]
     for i in range(len(poly)):
-        inte = (poly[i] ** i) / (i + 1)
+        inte = poly[i] / (i + 1)
         if i == 0:
             res.append(poly[i])
         elif inte.is_integer():
